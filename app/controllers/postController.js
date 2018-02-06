3f1d5ede2b35b458ad53e9c794f47ab92083cb7d
Stephen Cavell <cavell.stpehen@gmail.com>
2018-02-05 18:03:21 -0800

    update that was missed last commit

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -9,11 +9,11 @@ const Post = require('../models/post');
 //EXPORT ALL POSTS:
 exports.get_posts_list = function(req, res) {
 	
-		Post.find(function(err, posts) {
-			if(err)
-				res.send(err);
-			res.json(posts);
-		});
+	Post.find(function(err, posts) {
+		if(err)
+			res.send(err);
+		res.json(posts);
+	});
 };
 
 //EXPORT SINGLE POST:
